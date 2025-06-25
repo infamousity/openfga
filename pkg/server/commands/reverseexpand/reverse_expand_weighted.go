f2438116966a2eaba57a79b2d57c473bc1d31bf2
justin <justin.cohen@okta.com>
2025-06-25 08:45:07 -0400

    create buildUserFilter, checkQueryIsUnique, big readbility refactor

--- pkg/server/commands/reverseexpand/reverse_expand_weighted.go
+++ pkg/server/commands/reverseexpand/reverse_expand_weighted.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"sync"
+
+	"go.opentelemetry.io/otel/trace"
+
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	weightedGraph "github.com/openfga/language/pkg/go/graph"
-	"github.com/openfga/openfga/pkg/telemetry"
-	"go.opentelemetry.io/otel/trace"
-	"sync"
 
 	"github.com/openfga/openfga/internal/concurrency"
 	"github.com/openfga/openfga/internal/condition"
@@ -16,6 +17,7 @@ import (
 	"github.com/openfga/openfga/internal/utils"
 	"github.com/openfga/openfga/internal/validation"
 	"github.com/openfga/openfga/pkg/storage"
+	"github.com/openfga/openfga/pkg/telemetry"
 	"github.com/openfga/openfga/pkg/tuple"
 )
 
@@ -261,6 +263,7 @@ func (c *ReverseExpandQuery) queryForTuples(
 	// This map is used for memoization within this query path. It prevents re-running the exact
 	// same database query for a given object type, relation, and user filter.
 	jobDedupeMap := new(sync.Map)
+
 	var queryFunc func(context.Context, *ReverseExpandRequest, string) error
 	queryFunc = func(qCtx context.Context, r *ReverseExpandRequest, foundObject string) error {
 		if qCtx.Err() != nil {
@@ -270,9 +273,15 @@ func (c *ReverseExpandQuery) queryForTuples(
 		// Ensure we're always working with a copy
 		currentReq := r.clone()
 
-		userFilter, typeRel, ok := c.buildFiltersAndDedupe(jobDedupeMap, currentReq, foundObject)
+		userFilter := c.buildUserFilter(currentReq, foundObject)
+
+		// Now pop the top relation off of the stack for querying
+		typeRel := currentReq.stack.Pop().typeRel
+
+		// Ensure that we haven't already run this query
+		ok := c.checkQueryIsUnique(jobDedupeMap, userFilter, typeRel)
 		if !ok {
-			// this means we've run this exact query in this tree's path already
+			// this means we've run this exact query in this branch's path already
 			return nil
 		}
 
@@ -284,7 +293,7 @@ func (c *ReverseExpandQuery) queryForTuples(
 		}
 		defer filteredIter.Stop()
 
-		// TODO: this means EACH query could kick off resolveNodeBreadthLimit goroutines which defeats the purpose
+		// TODO: this means EACH query could start resolveNodeBreadthLimit goroutines which defeats the purpose of a limit
 		// Could manage the limit with select over a channel
 		pool := concurrency.NewPool(ctx, int(c.resolveNodeBreadthLimit))
 
@@ -352,19 +361,15 @@ func (c *ReverseExpandQuery) queryForTuples(
 	return nil
 }
 
-func (c *ReverseExpandQuery) buildFiltersAndDedupe(
-	dedupeMap *sync.Map,
+func (c *ReverseExpandQuery) buildUserFilter(
 	req *ReverseExpandRequest,
 	object string,
-) ([]*openfgav1.ObjectRelation, string, bool) {
-
-	var typeRel string
+) []*openfgav1.ObjectRelation {
 	var userFilter []*openfgav1.ObjectRelation
 
-	// This is true on every call except the first
+	// This is true on every call to queryFunc except the first
 	if object != "" {
-		entry := req.stack.Pop()
-		typeRel = entry.typeRel
+		entry := req.stack.Peek()
 		filter := &openfgav1.ObjectRelation{Object: object}
 		if entry.usersetRelation != "" {
 			filter.Relation = entry.usersetRelation
@@ -379,32 +384,18 @@ func (c *ReverseExpandQuery) buildFiltersAndDedupe(
 			userID = val.Object.GetId()
 		}
 
-		to := req.weightedEdge.GetTo()
+		toNode := req.weightedEdge.GetTo()
 
-		switch to.GetNodeType() {
+		switch toNode.GetNodeType() {
 		case weightedGraph.SpecificType: // Direct User Reference. To() -> "user"
-			typeRel = req.stack.Pop().typeRel
-			userFilter = append(userFilter, &openfgav1.ObjectRelation{Object: tuple.BuildObject(to.GetUniqueLabel(), userID)})
+			userFilter = append(userFilter, &openfgav1.ObjectRelation{Object: tuple.BuildObject(toNode.GetUniqueLabel(), userID)})
 
 		case weightedGraph.SpecificTypeWildcard: // Wildcard Referece To() -> "user:*"
-			typeRel = req.stack.Pop().typeRel
-			userFilter = append(userFilter, &openfgav1.ObjectRelation{Object: to.GetUniqueLabel()})
+			userFilter = append(userFilter, &openfgav1.ObjectRelation{Object: toNode.GetUniqueLabel()})
 		}
 	}
 
-	objectType, relation := tuple.SplitObjectRelation(typeRel)
-
-	// Create a unique key for the current query to avoid duplicate work.
-	key := utils.Reduce(userFilter, "", func(accumulator string, current *openfgav1.ObjectRelation) string {
-		return current.String() + accumulator
-	})
-	key += relation + objectType
-	if _, loaded := dedupeMap.LoadOrStore(key, struct{}{}); loaded {
-		// If this exact query has been run before in this path, abort.
-		return nil, "", false
-	}
-
-	return userFilter, typeRel, true
+	return userFilter
 }
 
 // buildFilteredIterator constructs the iterator used when reverse_expand queries for tuples.
@@ -435,4 +426,25 @@ func (c *ReverseExpandQuery) buildFilteredIterator(
 		validation.FilterInvalidTuples(c.typesystem),
 	)
 	return filteredIter, nil
+}
+
+func (c *ReverseExpandQuery) checkQueryIsUnique(
+	dedupeMap *sync.Map,
+	userFilter []*openfgav1.ObjectRelation,
+	typeRel string,
+) bool {
+	objectType, relation := tuple.SplitObjectRelation(typeRel)
+
+	// Create a unique key for the current query to avoid duplicate work.
+	key := utils.Reduce(userFilter, "", func(accumulator string, current *openfgav1.ObjectRelation) string {
+		return current.String() + accumulator
+	})
+
+	key += relation + objectType
+	if _, loaded := dedupeMap.LoadOrStore(key, struct{}{}); loaded {
+		// This means this query has been run on this branch of the graph already, don't do it again.
+		return false
+	}
+
+	return true
 }
